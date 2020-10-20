94065068650f5caa1af87c612d18d533d2cbea1c
Samuel Hick <samuel.hick@gmx.at>
2020-10-20 15:13:19 +0200

    removed html tags becasue they were causing javadoc errors

--- src/main/java/at/fhhagenberg/sqe/IElevator.java
+++ src/main/java/at/fhhagenberg/sqe/IElevator.java
@@ -3,72 +3,72 @@ package at.fhhagenberg.sqe;
 /**
  * High-level interface to access and control the elevator system.
  * 
- * <p>The elevator's behavior is impacted by the following parameters:
- * <ul><i>Elevator capacity.</i> The maximum number of passengers that can fit on an 
- * elevator.</ul>
- * <ul><i>Elevator speed.</i> The maximum speed an elevator can travel at in feet/sec. 
- * It is assumed to be the same for all elevators.</ul>
- * <ul><i>Elevator acceleration.</i> The rate at which the elevator can increase or decrease
+ * The elevator's behavior is impacted by the following parameters:
+ * Elevator capacity. The maximum number of passengers that can fit on an 
+ * elevator.
+ * Elevator speed. The maximum speed an elevator can travel at in feet/sec. 
+ * It is assumed to be the same for all elevators.
+ * Elevator acceleration. The rate at which the elevator can increase or decrease
  * speed in ft/sec2. It is assumed to be the same for all elevators. The higher the acceleration,
- * the faster the elevator can accelerate and decelerate, allowing its average speed to be faster.</ul>
- * <ul><i>Number of floors.</i> The number of floors in the building, including the ground floor. It is
+ * the faster the elevator can accelerate and decelerate, allowing its average speed to be faster.
+ * Number of floors. The number of floors in the building, including the ground floor. It is
  * assumed there are no floors below ground level. All elevators service the
  * ground floor but may, as described below, not necessarily service all other floors. Each floor has 
  * an up and a down button for the purposes of passengers calling the elevator. Floor numbering starts 
- * at 0 for floor 1.</ul> 
- * <ul><i>Floor height.</i> The height of the floors in feet. It is assumed that each floor is 
- * the same height.</ul>
- * <ul><i>Number of elevators.</i> The number of elevators in the building. Elevator numbering starts 
- * at zero for elevator 1.</ul>
+ * at 0 for floor 1. 
+ * Floor height. The height of the floors in feet. It is assumed that each floor is 
+ * the same height.
+ * Number of elevators. The number of elevators in the building. Elevator numbering starts 
+ * at zero for elevator 1.
  * 
- * <p>Some elevator behavior is inherent and cannot be controlled:
- * <ul><i>Door opening & closing.</i> Elevator doors automatically open when arriving at a floor and 
+ * Some elevator behavior is inherent and cannot be controlled:
+ * Door opening & closing. Elevator doors automatically open when arriving at a floor and 
  * close before departing. When passengers are waiting to board an elevator, the doors will 
- * stay open until all passengers are boarded. Each open and close action takes 1 second.</ul>
- * <ul><i>Elevator buttons.</i> Arriving passengers press the appropriate elevator button and the 
- * light is automatically turned off when the elevator arrives at the associated floor.</ul>
- * <ul><i>Elevator speed and acceleration.</i> The elevator will automatically accelerate to the maximum 
- * speed possible until there is only enough time left to slow down to arrive at the target floor.</ul>
+ * stay open until all passengers are boarded. Each open and close action takes 1 second.
+ * Elevator buttons. Arriving passengers press the appropriate elevator button and the 
+ * light is automatically turned off when the elevator arrives at the associated floor.
+ * Elevator speed and acceleration. The elevator will automatically accelerate to the maximum 
+ * speed possible until there is only enough time left to slow down to arrive at the target floor.
  * 
- * <p>There is substantial elevator status information available:
- * <ul><i>Elevator speed.</i> The current speed of the elevator. Positive speed indicates an elevator
- * heading up while negative indicates an elevator going down.</ul>
- * <ul><i>Elevator acceleration.</i> The acceleration of the elevators.</ul>
- * <ul><i>Elevator button status.</i> For each elevator, which floor buttons on the elevator have 
- * been pressed.</ul>
- * <ul><i>Elevator position.</i> The current position of the elevator, both in feet and to 
- * the closest floor. Position is measured in feet above ground floor with zero being the bottom.</ul>
- * <ul><i>Elevator door status.</i> Whether the doors for an elevator are open or closed. The 
- * status may also indicate a transition between open or closed.</ul>
- * <ul><i>Elevator target.</i> The current floor target of the elevator as set by the controller. The
- * elevator will travel to that target and stop until directed to the next target.</ul>
- * <ul><i>Elevator committed direction.</i> The current committed direction of the elevator. Elevators
+ * There is substantial elevator status information available:
+ * Elevator speed. The current speed of the elevator. Positive speed indicates an elevator
+ * heading up while negative indicates an elevator going down.
+ * Elevator acceleration. The acceleration of the elevators.
+ * Elevator button status. For each elevator, which floor buttons on the elevator have 
+ * been pressed.
+ * Elevator position. The current position of the elevator, both in feet and to 
+ * the closest floor. Position is measured in feet above ground floor with zero being the bottom.
+ * Elevator door status. Whether the doors for an elevator are open or closed. The 
+ * status may also indicate a transition between open or closed.
+ * Elevator target. The current floor target of the elevator as set by the controller. The
+ * elevator will travel to that target and stop until directed to the next target.
+ * Elevator committed direction. The current committed direction of the elevator. Elevators
  * responding to a passenger floor button must have a committed direction, up or down. Otherwise, 
  * passengers wishing to go up would board the same elevators as passengers wishing to go down. 
  * Accordingly, each elevator can have an up, down or uncommitted committed direction set in response 
- * to passenger travel..</ul>
- * <ul><i>Elevator floor service.</i> Whether a particular elevator services a particular floor. When 
+ * to passenger travel..
+ * Elevator floor service. Whether a particular elevator services a particular floor. When 
  * elevators are allowed to only service certain floors, this can help to achieve greater passenger 
- * service. Every elevator must service the ground floor.</ul>
- * <ul><i>Elevator weight.</i> This provides the current weight of the elevator less the weight of the 
+ * service. Every elevator must service the ground floor.
+ * Elevator weight. This provides the current weight of the elevator less the weight of the 
  * empty elevator – hence the weight of the passengers on board. This can be useful for detecting
- * when the elevator is getting full.</ul>
+ * when the elevator is getting full.
  * 
- * <p>The elevators can be controlled by setting the following parameters:
- * <ul><i>Elevator target.</i> Sets the floor target of a particular elevator. Typically, this target 
+ * The elevators can be controlled by setting the following parameters:
+ * Elevator target. Sets the floor target of a particular elevator. Typically, this target 
  * is set in response to a given passenger selecting a floor up or down button, or selecting a floor 
  * button in an elevator. The elevator will travel to that target and stop until directed to the 
- * next target.</ul>
- * <ul><i>Elevator committed direction.</i> Sets the current committed direction of the elevator. This 
+ * next target.
+ * Elevator committed direction. Sets the current committed direction of the elevator. This 
  * is set so that passengers board elevators going in their desired direction. This is indicated by the
  * controller by whether the up or down floor button has been pressed on a given floor. This is
  * an important concept and it should be recognized that in some cases, an elevator may be 
  * going down, for example, but its committed direction is up – reflecting the fact it has been 
  * dispatched down to pick up a passenger who is traveling up. Whenever an elevator is being
- * dispatched to a target floor, it should have a committed direction set.</ul>
- * <ul><i>Elevator floor service.</i> Sets whether a particular elevator services a particular floor. 
+ * dispatched to a target floor, it should have a committed direction set.
+ * Elevator floor service. Sets whether a particular elevator services a particular floor. 
  * In larger buildings, it is typically better to set elevator service to a particular subset of floors. 
- * Note that the ground floor will always get service.</ul>
+ * Note that the ground floor will always get service.
  *
  * @version 0.1
  */
