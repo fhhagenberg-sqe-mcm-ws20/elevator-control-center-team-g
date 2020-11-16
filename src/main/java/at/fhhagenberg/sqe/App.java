1aba6ad1c128cd1a157496d6348d6a41b8c28e52
Samuel <samuel.hick@gmx.at>
2020-11-16 13:42:09 +0100

    more work on UI

--- src/main/java/at/fhhagenberg/sqe/App.java
+++ src/main/java/at/fhhagenberg/sqe/App.java
@@ -1,8 +1,11 @@
 package at.fhhagenberg.sqe;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -40,50 +43,70 @@ public class App extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
         
-        HBox elevators =  (HBox) scene.lookup("#Elevators");
-        elevators.getChildren().add(FXMLLoader.load(getClass().getResource("/OneElevator.fxml")));
+        AddElevator(scene, 1);
+        AddElevator(scene, 2);
+        AddElevator(scene, 3);
+   
+       
         
-        VBox vbox = (VBox) elevators.getChildren().get(0);
+        
+
+        
+    }
+    
+    void AddElevator(Scene scene, int floornum) throws IOException {
+    	// get the part of the UI that contains all elevators
+    	HBox elevators =  (HBox) scene.lookup("#Elevators");
+    	
+    	// add new elevator to UI
+        elevators.getChildren().add(FXMLLoader.load(getClass().getResource("/OneElevator.fxml")));
+    	
+        // Get number of elevator
+        int Elevatornum = elevators.getChildren().size();
+        // get the vbox of this elevator
+        VBox vbox = (VBox) elevators.getChildren().get(Elevatornum - 1);
+        // set correct text to be displayed
+        Label lab = (Label) vbox.getChildren().get(0);
+        lab.setText("Elevator " + Elevatornum);
+        // get gridpane for this elevator
         ScrollPane scroll = (ScrollPane) vbox.getChildren().get(1);
         GridPane gp1 = (GridPane) scroll.getContent();
-        
-        //GridPane gp1 = (GridPane) elevators.getChildren().get(0).lookup("#GridpaneElevator1");
-   
+        // set ID to correct number
+        gp1.setId("GridpaneElevator" + Elevatornum);
+    	
+        // set size of grid elems
         gp1.getColumnConstraints().get(0).setMinWidth(100);
         gp1.getColumnConstraints().get(0).setMaxWidth(100);
         gp1.getColumnConstraints().get(1).setMinWidth(100);
         gp1.getColumnConstraints().get(1).setMaxWidth(100);
         
-        for (int i = 0; i < 10; i++) {
-        	AddFloorNumber(gp1, i);
+        // create floors for this elevator
+        for (int i = 0; i < floornum; i++) {
+        	AddFloorNumber(gp1, i, floornum - i);
 		}
-        
     }
     
-    void AddFloorNumber(GridPane gp, int rownr) {
+    void AddFloorNumber(GridPane gp, int rownr, int floornum) {
+    	// Create circle around floor number
     	Circle c1 = new Circle(40);
         c1.setFill(Paint.valueOf("WHITE"));
         c1.setStrokeWidth(5);
         c1.setStroke(Paint.valueOf("BLACK"));
         c1.setStrokeType(StrokeType.INSIDE);
-        
         gp.add(c1, 1, rownr);
         
         GridPane.setHalignment(c1, HPos.CENTER);
         GridPane.setValignment(c1, VPos.CENTER);
         
-        Text txt = new Text("0");
+        // create floor number
+        Text txt = new Text(Integer.toString(floornum));
         txt.setFont(Font.font(20));
-        
         gp.add(txt, 1, rownr);
-        
         GridPane.setHalignment(txt, HPos.CENTER);
         GridPane.setValignment(txt, VPos.CENTER);
         
+        // set fixed height
         gp.getRowConstraints().add(new RowConstraints(100));
-        
-       // gp.getRowConstraints().get(rownr).setMinHeight(100);
-        //gp.getRowConstraints().get(rownr).setMaxHeight(100);
     }
     
 }
