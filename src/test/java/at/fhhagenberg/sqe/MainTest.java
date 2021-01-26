52b0a3c2bb62c36e9d1dbd5bd9a5db27be0cc690
Samuel <samuel.hick@gmx.at>
2021-01-26 17:35:59 +0100

    added maintest for code coverage

---
+++ src/test/java/at/fhhagenberg/sqe/MainTest.java
@@ -0,0 +1,38 @@
+package at.fhhagenberg.sqe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.LabeledMatchers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
+import sqelevator.IElevator;
+
+@ExtendWith(ApplicationExtension.class)
+public class MainTest {
+	private Button button;
+	MockBuilding mockBuilding;
+	App app;
+
+	/**
+	 * @param robot - Will be injected by the test runner.
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testMainDoesntCrash(FxRobot robot) throws InterruptedException {
+		Main m = new Main();
+		TimeUnit.SECONDS.sleep(2);
+	}
+
+}
